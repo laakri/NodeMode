1c5573ac15728046378368a63636e9adcd963e36
SEIFEDDINE JLASSI <68915309+laakri@users.noreply.github.com>
2025-10-31 12:10:31 +0100

    adding the base nodemode to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nodemode/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
